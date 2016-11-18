325099de5db2980c74f9b0f970b2e746dccb3f93
= <=>
2016-11-17 18:06:07 -0600

    Vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+let routes = [
+  {path: '/', component: App}
+]
+
+let router = new VueRouter({routes})
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router
+}).$mount('#app')
