ec3d627607ea1e5fde0a7f502af309ddd52d06e0
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-11-29 12:03:29 -0600

    fix removed code

--- src/components/newUser/script/newUser.js
+++ src/components/newUser/script/newUser.js
@@ -1,9 +1,1 @@
-/* import baseRequest from '../../../lib/baseRequest'
 
-function login (username, password) {
-  return baseRequest.post('/Login', { username: username, password: password }, true)
-}
-
-export default {
-  requestLogin: login
-}*/
