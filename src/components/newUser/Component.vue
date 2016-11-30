2d71651e07b7d7bef15e6e7a8098e4afa95bcae7
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-11-29 21:43:58 -0600

    form groups

--- src/components/newUser/Component.vue
+++ src/components/newUser/Component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="new-user section">
+  <section class="new-user section">
     <div class="row">
       <h1>Create New User</h1>
     </div>
@@ -8,11 +8,21 @@
       <div class="panel panel-default">
         <div class="panel-body">
           <form v-on:submit.prevent="saveUser" class="col-sm-8 col-sm-offset-2">
-            <input type="text" class="form-control" name="username" id="username" value="" placeholder="Username" maxlength="15" v-model="username" />
-            <input type="text" class="form-control" name="password" id="password" value="" placeholder="Password" maxlength="15" v-model="password" />
-            <input type="text" class="form-control" name="firstname" id="firstname" value="" placeholder="First Name" maxlength="30" v-model="lastname" />
-            <input type="text" class="form-control" name="lastname" id="lastname" value="" placeholder="Last Name" maxlength="30" v-model="lastname" />
-            <input type="email" class="form-control" name="email" id="email" value="" placeholder="E-mail" maxlength="255" v-model="email" />
+            <div class="form-group">
+              <input type="text" class="form-control" name="username" id="username" value="" placeholder="Username" maxlength="15" v-model="username" />
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="password" id="password" value="" placeholder="Password" maxlength="15" v-model="password" />
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="firstname" id="firstname" value="" placeholder="First Name" maxlength="30" v-model="lastname" />
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="lastname" id="lastname" value="" placeholder="Last Name" maxlength="30" v-model="lastname" />
+            </div>
+            <div class="form-group">
+              <input type="email" class="form-control" name="email" id="email" value="" placeholder="E-mail" maxlength="255" v-model="email" />
+            </div>
             <hr>
             <input type="submit" class="btn btn-primary" id="saveButton" value="Save" />
             <div>
@@ -23,7 +33,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -56,5 +66,5 @@ export default {
 </script>
 
 <style scoped>
-  
+
 </style>
